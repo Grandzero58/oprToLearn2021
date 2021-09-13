e07d73d94e2497fc4f8ecc56857f5e31c5de83c3
ego <ego@ego>
2021-09-13 22:51:40 +0200

    Mit 202107.04 gestartet

---
+++ src/reko202107/a04_HashMap/Geldbetrag_Elmonarch.java
@@ -0,0 +1,20 @@
+package reko202107.a04_HashMap;
+
+import java.io.Reader;
+
+/**
+ *
+ * @author Elmonarch
+ */
+public class Geldbetrag_Elmonarch {
+    
+    public boolean sindGleich(Reader r1, Reader r2, short n){
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
